1a07d7fe38479048b0d4b09c2e441a7a37b2d717
Noam Szpiro <hellonoam@gmail.com>
2011-03-30 12:00:21 +0100

    added dot

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('notern - geo tagged notes\n');
+  res.end('notern - geo tagged notes.\n');
 }).listen(80, "64.30.141.213");
 console.log('Server running at http://64.30.141.213:80');
 
