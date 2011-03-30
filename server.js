694643b82bb425c6425537bc6b5bfea324b31724
Noam Szpiro <hellonoam@gmail.com>
2011-03-30 11:41:56 +0100

    changed ip

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(80, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:80/');
+}).listen(80, "64.30.141.213");
+console.log('Server running at http://64.30.141.213:80');
 
