1a9888040832079298c5204ae9933b2365c84e21
Sebastian <sebastian@kle.io>
2011-03-31 14:55:05 +0100

    Started note.js model class

---
+++ public/js/note.js
@@ -0,0 +1,50 @@
+/**
+ * Constructor for notes
+ * Checks the server for newer versions of the nodes
+ */
+function Note() {
+};
+
+
+/**
+ * Returns a note object for a particular key
+ * @params:
+ *   key : the key of the note
+ * @returns:
+ *   the note for the key or null if none exist
+ */
+Note.get = function(key) {
+  return null;
+};
+
+
+/**
+ * Returns a list of all locally stored notes
+ * @returns:
+ *   List of all the notes for a given user
+ */
+Note.get = function(key) {
+  return null;
+};
+
+
+/**
+ * Saves a node to the local datastore
+ * and to the server.
+ * @returns:
+ *    an array of notes
+ */
+Note.prototype.destroy = function() {
+  // save the node to the datastore
+  // and to the server
+};
+
+
+/**
+ * Destroys a particular note
+ * @void
+ */
+Note.prototype.save = function() {
+  // save the node to the datastore
+  // and to the server
+};
