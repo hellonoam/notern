7a0dd8388dbba2aba9e539c8226ae53d21c89a09
Noam Szpiro <hellonoam@gmail.com>
2011-03-31 13:22:44 +0100

    added express to server.js

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('notern - geo tagged notes!\n');
-}).listen(80, "0.0.0.0");
-console.log('Server running.');
 
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyDecoder());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(80);
+  console.log("Express server listening on port %d", app.address().port)
+}
