8941f5f45c0f7321b1fc6b066da837e703c104c0
Sebastian <sebastian@kle.io>
2011-03-31 13:51:34 +0100

    Refactored server.js

--- server.js
+++ server.js
@@ -4,44 +4,15 @@ require.paths.unshift(__dirname + "/vendor");
  * Module dependencies.
  */
 
-var express = require('express');
-	util    = require("util");
-
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyDecoder());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.staticProvider(__dirname + '/public'));
+process.addListener('uncaughtException', function (err, stack) {
+  console.log('------------------------');
+  console.log('Exception: ' + err);
+  console.log(err.stack);
+  console.log('------------------------');
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
+var Notern = require('./lib/notern');
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-// '/' with get or post gets the index file in the public folder
-app.get("/", index);
-app.post("/", index);
-function index(req, res) {
-	res.writeHead(200, {"content-type": "text/html"});
-	var rs = fs.createReadStream("./public/index.html");
-	util.pump(rs, res);
-}
-
-// Only listen on $ node app.js
-
-if (!module.parent) {
-  app.listen(80);
-  console.log("Express server listening on port %d", app.address().port)
-}
+new Notern({
+  port: 80
+}).listen();
