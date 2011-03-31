713cd2525a85078d781fcd65b728fa85f94f7196
Sebastian <sebastian@kle.io>
2011-03-31 13:11:16 +0100

    Added underscore to the public folder

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('notern - geo tagged notes!\n');
-}).listen(80, "64.30.141.213");
+}).listen(80, "0.0.0.0");
 console.log('Server running at http://64.30.141.213:80');
 
